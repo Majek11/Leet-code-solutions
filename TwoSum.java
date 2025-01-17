71381a6bd47c8d13e0a47bd9bd88d2dc2d019dbd
Majek11 <majeklekan9@gmail.com>
2025-01-17 13:32:47 +0100

    leet-code-two-sum

--- TwoSum.java
+++ TwoSum.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 
 public class TwoSum {
 
-  public int[] TwoSum(int[] nums, int target) {
-    //int number = nums.length;
+  public static int[] TwoSum(int[] nums, int target) {
       for ( int i = 0; i < nums.length; i++ ) {
         for ( int j = i + 1; j < nums.length; j++ ) {
           if ( nums[i] + nums[j] == target ) {
@@ -12,11 +11,12 @@ public class TwoSum {
           }
         }
       } 
-      return newArray;
+      return new int[]{};
   }
   public static void main(String[] args) {
-    int[] nums = {2,7,11,15};
+    int[] nums = {2,11,15,7};
     int target = 9;
-    System.out.println(newArray);
+    int[] newArray = TwoSum(nums, target);
+    System.out.println(Arrays.toString(newArray));
   }
 }
